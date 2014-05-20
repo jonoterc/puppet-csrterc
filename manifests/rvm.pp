60ec987d312277712467de2208eaee30bcf6fd7d
jonoterc <jon_obuchowski@terc.edu>
2014-05-20 11:20:56 -0400

    fixing ordering around rvm/rvmrc

--- manifests/rvm.pp
+++ manifests/rvm.pp
@@ -1,24 +1,27 @@
 class csrterc::rvm (
-    $rvm_version = undef ,
+    $rvm_version  = undef ,
     $default_ruby = undef ,
   ) {
 
   class { '::rvm':
     version => $rvm_version ,
   }
-  
+
   class { '::rvm::rvmrc':
     max_time_flag => '20' ,
+    before        => [
+      Class['rvm'] ,
+    ]
   }
 
   if $default_ruby {
-  	rvm_system_ruby { $default_ruby :
-    	ensure => 'present' ,
-    	default_use => true ,
-      require => [
-      	Class['::rvm'] ,
+    rvm_system_ruby { $default_ruby :
+      ensure      => 'present' ,
+      default_use => true ,
+      require     => [
+        Class['::rvm'] ,
       ] ,
-	  }
+    }
   }
-  
+
 }

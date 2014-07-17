2c39a3e2644d69bbfb5855ebbd3c75fdc4d3b080
jonoterc <jon_obuchowski@terc.edu>
2014-07-17 14:09:01 -0400

    updating mysql default config to use "utf8" for default charset and "utf8_unicode_ci" for default collation (stock mysql module defaults collation to "utf8_general_ci")

--- manifests/user/mysql_db.pp
+++ manifests/user/mysql_db.pp
@@ -1,10 +1,14 @@
 define csrterc::user::mysql_db (
     $user_name = $title ,
+    $charset     = 'utf8',
+    $collate     = 'utf8_unicode_ci',
   ) {
 
   mysql_database { $user_name:
-    ensure     => 'present' ,
+    ensure     => 'present',
     provider   => 'mysql',
+    charset    => $charset,
+    collate    => $collate,
     require    => [ Class['mysql::server'] ],
   }
 
